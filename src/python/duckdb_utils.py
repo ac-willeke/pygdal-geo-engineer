8fe1ec9e647afdbf2ed940dfe4f9e2b9cde38a75
wilaca-nina.gis <willeke.acampo@nina.no>
2024-03-11 11:41:35 +0100

    add: sum of ar50, difference

--- src/python/duckdb_utils.py
+++ src/python/duckdb_utils.py
@@ -52,13 +52,32 @@ def load_gpkg_layers(db_path, gpkg_path, layer_name):
             print(f"File {parquet_path} does not exist. Skipping.")
     return
 
-def remove_duckdb_database(db_path):
+def remove_database(db_path):
     if os.path.exists(db_path):
         os.remove(db_path)
     else:
         print(f"The file {db_path} does not exist")
     return
 
+def remove_table(db_path, table_name):
+    with duckdb.connect(database=db_path, read_only=False) as con:
+    
+        # if the table exists, drop it
+        con.sql(f"DROP TABLE {table_name}")
+    return
+
+def field_exists(db_path, table_name, field_name):
+    with duckdb.connect(database=db_path, read_only=False) as con:
+        result = con.execute(f"PRAGMA table_info({table_name})").fetchdf()
+        return field_name in result['name'].values
+
+def remove_field(db_path, table_name, field_name):
+    with duckdb.connect(database=db_path, read_only=False) as con:
+        
+        # if the field exists, drop it
+        if field_exists(db_path, table_name, field_name):
+            con.sql(f"ALTER TABLE {table_name} DROP COLUMN {field_name}")
+    return
 
 def print_duckdb_info(db_path):
     with duckdb.connect(database=db_path, read_only=False) as con:
@@ -72,7 +91,6 @@ def print_duckdb_info(db_path):
         ).fetchall()
         print("Tables:", tables)
         
-
     return
 
 def ar50_area_class(db_path: str, tbl_study_area: str, id: str, tbl_ar50: str, ar50_field: str, area_class: Union[int, List[int]], new_field: str) -> None:
@@ -130,6 +148,114 @@ def ar50_area_class(db_path: str, tbl_study_area: str, id: str, tbl_ar50: str, a
             """)
     except Exception as e:
         print(f"An error occurred: {e}")
+    
+def sum_area_cols(db_path: str, tbl_name: str, id: str, area_fields: List[str], new_field: str) -> None:
+    """ 
+    Calculate the sum of areas for a list of area fields, and store the result in a new field.
+    
+    Args:
+        db_path (str): Path to the database.
+        tbl_name (str): Name of the table.
+        id (str): ID field name.
+        area_fields (List[str]): List of area fields.
+        new_field (str): Name of the new field to store the result.
+    """
+    
+    try: 
+        with duckdb.connect(database=db_path, read_only=False) as conn:
+            # spatial extension
+            conn.sql("INSTALL spatial;")
+            conn.sql("LOAD spatial;")
+            
+            # Create a temp table with the sum of the areas
+            conn.sql(f"""
+                CREATE TEMPORARY TABLE tbl_sum_areas AS
+                SELECT
+                    {id},
+                    {', '.join(area_fields)},
+                    {'+'.join(area_fields)} as sum
+                FROM
+                    {tbl_name}
+            """)
+            
+            ## add the new field to the study area table
+            conn.sql(f"""
+                ALTER TABLE {tbl_name}
+                ADD COLUMN {new_field} REAL
+            """)
+
+            # Update the field in the original table with the sum of the areas
+            conn.sql(f"""
+                UPDATE {tbl_name} as study_area
+                SET {new_field} = tmp_areas.sum
+                FROM tbl_sum_areas as tmp_areas
+                WHERE study_area.{id} = tmp_areas.{id}
+            """)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
+def geom_area(db_path: str, tbl_name: str, geom_field: str, area_field: str) -> None:
+    """
+    Calculate the area of the geometry field and store the result in a new field.
+
+    Args:
+        db_path (str): Path to the database.
+        tbl_name (str): Name of the table.
+        geom_field (str): Name of the geometry field.
+        new_field (str): Name of the new field to store the result.
+    """
+    
+    try: 
+        with duckdb.connect(database=db_path, read_only=False) as conn:
+            # spatial extension
+            conn.sql("INSTALL spatial;")
+            conn.sql("LOAD spatial;")
+            
+            # add the area field to the study area table
+            conn.sql(f"""
+                ALTER TABLE {tbl_name}
+                ADD COLUMN {area_field} REAL
+            """)
+
+            # calculate the area    
+            conn.sql(f"""
+                UPDATE {tbl_name} as study_area
+                SET {area_field} = ST_Area({geom_field})
+            """)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def area_difference(db_path: str, tbl_name: str, field_a: str, field_b: str, area_diff_field: str) -> None:
+    """
+    Calculate the area difference between two area fields and store the result in a new field.
+    
+    Args:
+        db_path (str): Path to the database.
+        tbl_name (str): Name of the table.
+        field_a (str): Name of the first area field.
+        field_b (str): Name of the second area field.
+        area_diff_field (str): Name of the new field to store the result.
+    """
+    
+    try: 
+        with duckdb.connect(database=db_path, read_only=False) as conn:
+            # spatial extension
+            conn.sql("INSTALL spatial;")
+            conn.sql("LOAD spatial;")
+            
+            # add the area field to the study area table
+            conn.sql(f"""
+                ALTER TABLE {tbl_name}
+                ADD COLUMN {area_diff_field} REAL
+            """)
+
+            # calculate the area difference    
+            conn.sql(f"""
+                UPDATE {tbl_name} as study_area
+                SET {area_diff_field} = {field_a} - {field_b}
+            """)
+    except Exception as e:
+        print(f"An error occurred: {e}")       
         
 def blob_to_geom(db_path, tbl_name, blob_field, geom_field):
     """ Convert a BLOB field to a geometry field.
