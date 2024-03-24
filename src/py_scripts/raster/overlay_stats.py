cf7ac886b172f8f525bf0f4908c80cf4b15096d9
wilaca-nina.gis <willeke.acampo@nina.no>
2024-03-24 22:48:41 +0100

    debug: remove buffer from geom, only use for calc

--- src/py_scripts/raster/overlay_stats.py
+++ src/py_scripts/raster/overlay_stats.py
@@ -10,18 +10,22 @@ import rasterio
 def overlay_stats(raster_path, vector_gdf, buffer_distance) -> gpd.GeoDataFrame:
     
     if buffer_distance > 0:
-        vector_gdf['geometry'] = vector_gdf.buffer(buffer_distance)
+        gdf = vector_gdf.copy()
+        gdf['geometry'] = gdf.buffer(buffer_distance)
     
     # calc: min, max, mean, std, median
     stats = zonal_stats(
-        vector_gdf, 
+        gdf, 
         raster_path, 
         stats=['min', 'max', 'mean', 'std', 'median']
         )
-    vector_gdf['infra_min'] = [stat['min'] for stat in stats]
-    vector_gdf['infra_max'] = [stat['max'] for stat in stats]
-    vector_gdf['infra_mean'] = [stat['mean'] for stat in stats]
-    vector_gdf['infra_std'] = [stat['std'] for stat in stats]   
-    vector_gdf['infra_median'] = [stat['median'] for stat in stats]
-
+    gdf['infra_min'] = [stat['min'] for stat in stats]
+    gdf['infra_max'] = [stat['max'] for stat in stats]
+    gdf['infra_mean'] = [stat['mean'] for stat in stats]
+    gdf['infra_std'] = [stat['std'] for stat in stats]   
+    gdf['infra_median'] = [stat['median'] for stat in stats]
+    
+    # join the results back to the original geodataframe
+    vector_gdf = vector_gdf.join(gdf[['infra_min', 'infra_max', 'infra_mean', 'infra_std', 'infra_median']])
+    
     return vector_gdf  
